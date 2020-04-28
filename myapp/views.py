06974f8d5236b33edb348e055c088638975c401a
quinchoponcho <javalle.gutierrez@gmail.com>
2020-04-28 14:14:11 -0600

    202004281414

--- myapp/views.py
+++ myapp/views.py
@@ -21,3 +21,20 @@ def index(request):
 
     return render(request, 'index.html', context_dict)
 
+def dashBoard(request):
+	orders = Order.objects.all().order_by('-status')[0:5]
+	customers = Customer.objects.all()
+
+	total_customers = customers.count()
+
+	total_orders = Order.objects.all().count()
+	delivered = Order.objects.filter(status='Delivered').count()
+	pending = Order.objects.filter(status='Pending').count()
+
+
+
+	context = {'customers':customers, 'orders':orders,
+	'total_customers':total_customers,'total_orders':total_orders, 
+	'delivered':delivered, 'pending':pending}
+	return render(request, 'accounts/dashBoard.html', context)
+
