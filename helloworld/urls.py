651db1bb21f1000454087d29bb5be50e5ca9dadb
quinchoponcho <javalle.gutierrez@gmail.com>
2020-04-28 12:57:36 -0600

    202004281257

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+url(r'^$', views.index, name='index'),
+url(r'^myapp/', include('myapp.urls')),
+url(r'^admin/', admin.site.urls),
 ]
